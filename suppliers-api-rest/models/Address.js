1ed96b8c160892e7da001da5c07a57a26007da56
Fede <Federico@bankingly.com>
2022-10-04 18:59:53 -0300

    Three api modules created and some initial models for suppliers api.

---
+++ suppliers-api-rest/models/Address.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const addressSchema = new mongoose.Schema({
+  country: {
+    type: String,
+    required: true,
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+});
+
+mongoose.model("Address", addressSchema);
---
+++ suppliers-api-rest/models/Supplier.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+const Address = require("./Address");
+
+const supplierSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: Address,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  integrationURL: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  // This has to be encrypted?
+  privateKey: {
+    type: String,
+    required: true,
+  },
+});
+
+mongoose.model("Supplier", supplierSchema);
