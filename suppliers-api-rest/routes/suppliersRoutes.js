8b1c8c62c6874c3a040053e47ae432c0dcd29e65
Fede <Federico@bankingly.com>
2022-10-17 19:10:56 -0300

    Moving generation of public/private keys to another module.

--- suppliers-api-rest/routes/suppliersRoutes.js
+++ suppliers-api-rest/routes/suppliersRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const Supplier = mongoose.model('Supplier');
-const { generateKeyPair } = require('crypto');
+const generateKeys = require('../common/cryptographer');
 const axios = require('axios');
 
 var constants = require('../models/Constants');
@@ -10,38 +10,15 @@ const router = express.Router();
 
 router.post('/supplier', async (req, res) => {
 	try {
-		let supplierPrivateKey = '';
-		let supplierPublicKey = '';
-		generateKeyPair(
-			'ec',
-			{
-				namedCurve: 'secp256k1', // Options
-				publicKeyEncoding: {
-					type: 'spki',
-					format: 'der',
-				},
-				privateKeyEncoding: {
-					type: 'pkcs8',
-					format: 'der',
-				},
-			},
-			(err, publicKey, privateKey) => {
-				if (!err) {
-					supplierPrivateKey = privateKey.toString('hex');
-					supplierPublicKey = publicKey.toString('hex');
-				} else {
-					console.log('Errr is: ', err);
-				}
-			}
-		);
+		const { publicKey, privateKey } = generateKeys();
 		const supplier = new Supplier(
 			({ name, addres, email, phone, integrationURL } = req.body)
 		);
 
-		supplier.privateKey = supplierPrivateKey;
+		supplier.privateKey = privateKey;
 
 		await supplier.save();
-		res.status(200).send({ publicKey: supplierPublicKey });
+		res.status(200).send({ publicKey: publicKey });
 	} catch (err) {
 		return res.status(422).send({
 			status: constants.RESPONSE_STATUS_ERROR,
