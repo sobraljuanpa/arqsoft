c5ffd3f56401156b28aa4045989421a6dae37b4a
Juan Pablo Sobral <sobraljuanpa@gmail.com>
2022-11-18 21:11:29 -0300

    Refactored middleware into centralized package

---
+++ middleware/logs/activity.js
@@ -0,0 +1,31 @@
+const redis = require('redis');
+const publisher = redis.createClient({
+	socket: {
+		host: 'redis',
+		port: 6379,
+	},
+});
+publisher.connect();
+
+const logActivity = async (req, res, next) => {
+    let inboundTimestamp = new Date();
+	res.on('finish', async function () {
+		let outboundTimestamp = new Date();
+		let diff =
+			outboundTimestamp.getMilliseconds() - inboundTimestamp.getMilliseconds();
+		await publisher.publish(
+			'request',
+			JSON.stringify({
+				method: req.method,
+				url: req.originalUrl,
+				body: req.body,
+				statusCode: this.statusCode,
+				timetaken: diff,
+				timestamp: inboundTimestamp.toUTCString(),
+			})
+		);
+	});
+	next();
+}
+
+module.exports = { logActivity };
